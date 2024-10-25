582fc370fa8ce7d55c5a3e0db2c983648b14c0b6
Festus <techarenaz01@gmail.com>
2024-10-24 19:44:55 -0700

    added google icon to the img and imported it in the file. added more ui component to the userAuthForm page

--- client/src/pages/userAuthForm.page.jsx
+++ client/src/pages/userAuthForm.page.jsx
@@ -1,7 +1,6 @@
-
-
 import React from 'react'
-import InputBox from '../components/input.component'
+import InputBox from '../components/input.component';
+import googleIcon from '../imgs/google.png';
 
 function UserAuthForm({ type }) {
     return (
@@ -43,6 +42,17 @@ function UserAuthForm({ type }) {
                 type='submit'
                 >
                     {type == 'sign-in' ? "Sign in" : "Sign up"}
+                </button>
+
+                <div className='flex items-center gap-2 uppercase opacity-20 font-semibold mx-auto'>
+                    <hr className='w-[50%] border-black' />
+                    <p>or</p>
+                    <hr className='w-[50%] border-black' />
+                </div>
+
+                <button className='btn-dark flex items-center justify-center gap-4 w-[90%] mx-auto'>
+                    <img src={ googleIcon } alt="" className='w-5' />
+                    Connect With Google
                 </button>
 
             </form>
